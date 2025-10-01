034a3930b1b39f1469103945cbab49c104a5ede4
Bhavy-Kakaniya <bhavy2kakaniya@gmail.com>
2025-10-01 12:03:10 +0530

    getter setter using employee demo

---
+++ Learning/Class/getter_setter.java
@@ -0,0 +1,28 @@
+class MyEmployee {
+    private int id;
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String n) {
+        this.name = n;
+    }
+
+    public void setId(int i) {
+        this.id = i;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
+
+public class Employee {
+    public static void main(String[] args) {
+        MyEmployee emp1 = new MyEmployee();
+        emp1.setName("abcde");
+        System.out.println(emp1.getName());
+    }
+}
