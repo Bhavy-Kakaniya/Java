53e6ec497234d386adb8330ea9dd0695d28eb0cc
Bhavy-Kakaniya <bhavy2kakaniya@gmail.com>
2025-10-06 09:59:31 +0530

    handling packages

---
+++ Learning/Package/p3.java
@@ -0,0 +1,8 @@
+import pkgdemo.p2;
+public class p3 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        p2 k = new p2();
+        System.out.println("Class created");
+    }
+}
