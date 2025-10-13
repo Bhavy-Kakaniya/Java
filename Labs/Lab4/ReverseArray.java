fc0397b846b12db132b7f37416ad8e65d152c353
Bhavy-Kakaniya <bhavy2kakaniya@gmail.com>
2025-10-13 20:57:40 +0530

    reverse array

---
+++ Labs/Lab4/ReverseArray.java
@@ -0,0 +1,15 @@
+public class ReverseArray {
+    public static void main(String[] args) {
+        int[] array = { 10, 20, 30, 40, 50 };
+
+        System.out.println("enter : ");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+
+        System.out.println("Reverse :");
+        for (int i = array.length - 1; i >= 0; i--) {
+            System.out.print(array[i] + " ");
+        }
+    }
+}
