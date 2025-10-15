748f21b0284e01918030d4cd26582b8e3f87838d
Bhavy-Kakaniya <bhavy2kakaniya@gmail.com>
2025-10-15 19:44:05 +0530

    Solution

---
+++ Labs/lab5/05Solution.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements in the array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter the elements of the array (sorted and rotated): ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter the target value: ");
+        int target = sc.nextInt();
+
+        int left = 0, right = arr.length - 1;
+        int result = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target) {
+                result = mid;
+                break;
+            }
+
+            if (arr[left] <= arr[mid]) {
+                if (target >= arr[left] && target < arr[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            else {
+                if (target > arr[mid] && target <= arr[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        System.out.println("The index of the target is: " + result);
+    }
+}
