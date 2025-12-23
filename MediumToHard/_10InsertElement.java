4169875fbd08313966c6680f86696c015689945e
Bhavy-Kakaniya <bhavy2kakaniya@gmail.com>
2025-12-23 22:06:52 +0530

    insert element at given index

---
+++ MediumToHard/_10InsertElement.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class _10InsertElement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n + 1];
+
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter element to insert: ");
+        int element = sc.nextInt();
+
+        System.out.print("Enter position (1 to " + (n + 1) + "): ");
+        int pos = sc.nextInt();
+
+        // shifting elements to right
+        for (int i = n; i >= pos; i--) {
+            arr[i] = arr[i - 1];
+        }
+
+        arr[pos - 1] = element; // insert element at position
+
+        System.out.println("Array after insertion:");
+        for (int i = 0; i <= n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
